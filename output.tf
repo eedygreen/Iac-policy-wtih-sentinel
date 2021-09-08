12ff8d847a6788b7f5786032c88eb4f1b29a3a91
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-09-08 15:16:40 +0100

    fixed website endpoint

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
 
 output "s3_bucket_endpoint" {
   description = "The bucket endpoint"
-  value       = aws_s3_bucket.s3_bucket.s3_bucket_endpoint
+  value       = aws_s3_bucket.s3_bucket.website_endpoint
 }
 
 output "s3_bucket_endpoint_url" {
   description = "The bucket endpoint"
-  value       = "https://${aws_s3_bucket.s3_bucket.s3_bucket_endpoint}/index.html"
+  value       = "https://${aws_s3_bucket.s3_bucket.website_endpoint}/index.html"
 }
